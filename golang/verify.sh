d6cb5e41ce5632e7180f6519d197d573991867d6
xiangxinyong <xiangxingyong@qq.com>
2019-10-23 15:04:57 +0800

    add env

--- golang/verify.sh
+++ golang/verify.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+export GOPATH=$WORKSPACE
+
 go get golang.org/x/crypto/ssh
 go install golang.org/x/crypto/ssh
 go get golang.org/x/tools/cmd/goimports
 go install golang.org/x/tools/cmd/goimports
+
+export PATH=$PATH:$WORKSPACE/bin
 
 #go vet ./...
  
