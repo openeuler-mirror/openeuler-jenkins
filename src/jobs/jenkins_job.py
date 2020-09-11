866b8ae3698e5467556f552444894da0237c42f5
root <root@localhost.localdomain>
2020-09-11 09:13:41 +0800

    support auto create job for openeuler

--- src/jobs/jenkins_job.py
+++ src/jobs/jenkins_job.py
@@ -5,9 +5,11 @@ monkey.patch_all()
 
 from abc import ABCMeta, abstractmethod
 import os
+import stat
 import logging.config
 import logging
 import time
+import re
 import xml.etree.ElementTree as ET
 import yaml
 import argparse
@@ -181,21 +183,92 @@ class SrcOpenEulerJenkinsJobs(JenkinsJobs):
         return ET.tostring(root)
 
 
-class OpenEulerJenkinsJobs(JenkinsJobs):
+class OpenEulerJenkinsJobs(SrcOpenEulerJenkinsJobs):
     """
     openEuler 仓库
-    TODO...
     """
-    def get_real_target_jobs(self, jobs):
-        super(OpenEulerJenkinsJobs, self).get_real_target_jobs(jobs)
+    def guess_build_script(self, job):
+        """
+        返回仓库对应的jenkins build脚本
+        :param job: 
+        :return:
+        """
+        jenkinsfile_dir = os.path.realpath(os.path.join(__file__, "../../jenkinsfile"))
+
+        script = job
+        for filename in os.listdir(jenkinsfile_dir):
+            if filename == script:
+                break
+            if re.match("{}\..*".format(job), filename):   # repo.{sufix}
+                script = filename
+                break
+
+        script = os.path.realpath(os.path.join(jenkinsfile_dir, script))
+
+        # helper chmod +x
+        if os.path.exists(script):
+            st = os.stat(script)
+            os.chmod(script, st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+
+        return script
 
     def update_config(self, job):
-        pass
+        """
+        根据模板生成目标任务配置信息
+        :param job: 目标任务
+        :return: xml string
+        """
+        root = ET.fromstring(self._template_job_config.encode("utf-8"))
+
+        buddy = self._buddy_info[job]  #
+
+        # triggers
+        ele = root.find("triggers//regexpFilterExpression")
+        if ele is not None:
+            ele.text = ele.text.replace(self._template_job, buddy["repo"])
+
+        # parameterized trigger
+        ele = root.find("publishers/hudson.plugins.parameterizedtrigger.BuildTrigger//projects")
+        if ele is not None:
+            arches = self._exclusive_arch.get(buddy["repo"])
+            if arches:  # eg: [x86_64]
+                projects = []
+                for project in ele.text.split(","):
+                    for arch in arches:
+                        if arch in project:
+                            projects.append(project)
+                ele.text = ",".join(projects).replace(self._template_job, buddy["repo"])
+            else:
+                ele.text = ele.text.replace(self._template_job, buddy["repo"])
+
+        # build
+        script = self.guess_build_script(buddy["repo"])
+        logger.debug("guess build script: {}".format("script"))
+        ele = root.findall("buiders/hudson.task.Shell/command")
+        if ele:
+            # replace first command
+            command = ele[0]
+            command.text = script
+
+        # join trigger
+        ele = root.find("publishers/join.JoinTrigger//projects")
+        if ele is not None:
+            ele.text = ele.text.replace(self._template_job, buddy["repo"])
+
+        # set repo defaultValue
+        ele = root.find("properties//*[name=\"repo\"]/defaultValue")
+        if ele is not None:
+            ele.text = buddy["repo"]
+
+        return ET.tostring(root)
 
 
 if "__main__" == __name__:
     args = argparse.ArgumentParser()
-    args.add_argument("-a", type=str, dest="action", help="workspace where to find source")
+
+    args.add_argument("-f", type=str, dest="community", default="src-openeuler", help="src-openeuler or openeuler")
+
+    args.add_argument("-a", type=str, dest="action", help="create or update")
     args.add_argument("-c", type=int, dest="concurrency", default=75, help="jobs that send to jenkins server concurrency")
     args.add_argument("-r", type=int, dest="retry", default=3, help="retry times")
     args.add_argument("-i", type=int, dest="interval", default=0, help="retry interval")
@@ -206,10 +279,10 @@ if "__main__" == __name__:
     args.add_argument("-d", type=str, dest="target_job_base_url", help="jenkins base url of target jobs")
     args.add_argument("-o", type=int, dest="jenkins_timeout", default=10, help="jenkins api timeout")
 
-    args.add_argument("-u", type=str, dest="jenkins_user", help="repo name")
+    args.add_argument("-u", type=str, dest="jenkins_user", help="jenkins user name")
     args.add_argument("-t", type=str, dest="jenkins_api_token", help="jenkins api token")
 
-    args.add_argument("-x", type=str, dest="buddy_info_file", help="src-openeuler buddy info file")
+    args.add_argument("-x", type=str, dest="mapping_info_file", help="mapping info file")
     args.add_argument("-e", type=str, dest="exclusive_arch_file", help="exclusive arch file")
 
     args = args.parse_args()
@@ -224,5 +297,9 @@ if "__main__" == __name__:
     jp_m = JenkinsProxy(args.template_job_base_url, args.jenkins_user, args.jenkins_api_token, args.jenkins_timeout)
     jp_t = JenkinsProxy(args.target_job_base_url, args.jenkins_user, args.jenkins_api_token, args.jenkins_timeout)
 
-    jenkins_jobs = SrcOpenEulerJenkinsJobs(args.template_job, jp_m, args.buddy_info_file, args.exclusive_arch_file)
+    if args.community == "src-openeuler":
+        jenkins_jobs = SrcOpenEulerJenkinsJobs(args.template_job, jp_m, args.mapping_info_file, args.exclusive_arch_file)
+    else:
+        jenkins_jobs = OpenEulerJenkinsJobs(args.template_job, jp_m, args.mapping_info_file, args.exclusive_arch_file)
+
     jenkins_jobs.run(args.action, args.target_jobs, jp_t, concurrency=args.concurrency, retry=args.retry, interval=args.interval)
