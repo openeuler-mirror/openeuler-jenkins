81204e28f71b9ba93fe20c31a832bb8657572e33
liheavy <lihaiwei8@huawei.com>
2022-01-22 16:47:30 +0800

    将门禁脚本集成到openeuler-jenkins中

---
+++ src/lib/comment.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+. /home/jenkins/ci_check/src/lib/lib.sh
+
+owner="src-openeuler"
+jenkins_api_host="http://jenkins.jenkins"
+check_item_comment_aarch64=""
+check_item_comment_x86=""
+compare_package_result_aarch64=""
+compare_package_result_x86=""
+#需要输入的参数
+repo=$1
+prid=$2
+committer=$3
+giteetoken=$4
+jenkins_user=$5
+jenkins_api_token=$6
+WORKSPACE=$7
+SaveBuildRPM2Repo=$8
+repo_server=$9
+compare_result=${10}
+commentid=${11}
+
+# 清理环境
+function clearn_env() {
+  log_info "***** Start to clearn env *****"
+  # download compare package comment files
+  check_item_comment_aarch64="${repo}_aarch64_comment"
+  check_item_comment_x86="${repo}_x86_64_comment"
+  #cat $compare_package_comment_x86
+  compare_package_result_aarch64="${repo}_aarch64_compare_result"
+  compare_package_result_x86="${repo}_x86_64_compare_result"
+
+  if [[ -e check_item_comment_aarch64 ]]; then
+    rm $check_item_comment_aarch64
+  fi
+  if [[ -e $check_item_comment_x86 ]]; then
+    rm $check_item_comment_x86
+  fi
+  if [[ -e $compare_package_result_aarch64 ]]; then
+    rm $compare_package_result_aarch64
+  fi
+  if [[ -e $compare_package_result_x86 ]]; then
+    rm $compare_package_result_x86
+  fi
+  log_info "***** End to clearn env *****"
+}
+
+# 从文件服务器拷贝文件
+function scp_comment_file() {
+  log_info "***** Start to scp comment file *****"
+  fileserver_tmpfile_path="/repo/soe/check_item"
+  scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${check_item_comment_aarch64} . || log_info "file ${check_item_comment_aarch64} not exist"
+  scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${check_item_comment_x86} . || log_info "file ${check_item_comment_x86} not exist"
+  #ls $WORKSPACE/${comment}
+  scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${compare_package_result_aarch64} . || log_info "file ${compare_package_result_aarch64} not exist"
+  scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${compare_package_result_x86} . || log_info "file ${compare_package_result_x86} not exist"
+  ls $WORKSPACE/${compare_result}
+  log_info "***** End to scp comment file *****"
+}
+
+# 执行评论功能
+function exec_comment() {
+  log_info "***** Start to exec comment *****"
+  export PYTHONPATH=/home/jenkins/ci_check
+  python3 /home/jenkins/ci_check/src/build/gitee_comment.py -o $owner -r $repo -p $prid -c $committer -t ${giteetoken} -b $jenkins_api_host -u $jenkins_user -j $jenkins_api_token -a ${check_item_comment_aarch64} ${check_item_comment_x86} -f ${compare_package_result_x86},${compare_package_result_aarch64} -m ${commentid}
+  log_info "***** End to exec comment *****"
+}
+
+# 执行入口
+function main() {
+  clearn_env
+  scp_comment_file
+  exec_comment
+}
---
+++ src/lib/main_process.sh
@@ -0,0 +1,330 @@
+#!/bin/bash
+. /home/jenkins/ci_check/src/lib/lib.sh
+JENKINS_HOME=/home/jenkins
+SCRIPT_PATCH=${JENKINS_HOME}/ci_check/src/build
+BUILD_ROOT=${JENKINS_HOME}/agent/buildroot
+RPM_PATH=${BUILD_ROOT}/home/abuild/rpmbuild/RPMS
+comment_file=""
+# 需要输入的参数
+OBSUserName=$1
+OBSPassword=$2
+GiteeUserName=$3
+GiteePassword=$4
+GiteeToken=$5
+GiteeUserPassword=$6
+PkgShipUserName=$7
+PkgShipPassword=$8
+buddy=$9
+repo=${10}
+tbranch=${11}
+arch=${12}
+package=${13}
+branch=${14}
+prid=${15}
+WORKSPACE=${16}
+committer=${17}
+commentid=${18}
+SaveBuildRPM2Repo=${19}
+repo_server=${20}
+pkgship_notify_url=${21}
+pkgship_notify_token=${22}
+
+#配置osc
+function config_osc() {
+  log_info "***** Start to config osc *****"
+  cat >${JENKINS_HOME}/.oscrc <<EOF
+[general]
+apiurl = http://117.78.1.88
+no_verify = 1
+build-root = ${BUILD_ROOT}
+
+[http://117.78.1.88]
+user = ${OBSUserName}
+pass = ${OBSPassword}
+trusted_prj = openEuler:22.03:LTS:Next:selfbuild:BaseOS openEuler:20.03:LTS:SP3:selfbuild:BaseOS openEuler:selfbuild:BaseOS openEuler:20.03:LTS:selfbuild:BaseOS openEuler:selfbuild:function openEuler:20.09:selfbuild:BaseOS openEuler:20.03:LTS:SP1:selfbuild:BaseOS openEuler:21.03:selfbuild:BaseOS openEuler:20.03:LTS:SP2:selfbuild:BaseOS openEuler:21.09:selfbuild:BaseOS openEuler:20.03:LTS:Next:selfbuild:BaseOS # 不用输0,1,2了
+EOF
+  log_info "***** End to config osc *****"
+}
+
+#配置华为云maven源，备用镜像源https://mirrors.huaweicloud.com/repository/maven/
+#编译过程会将.m2文件内容复制到maven的配置文件中
+function config_maven() {
+  log_info "***** Start to config maven *****"
+  cat >${JENKINS_HOME}/.m2 <<EOF
+<settings>
+    <mirrors>
+        <mirror>
+            <id>huaweimaven</id>
+            <mirrorOf>*</mirrorOf>
+      		  <name>huaweicloud maven</name>
+      		  <url>https://repo.huaweicloud.com/repository/maven/</url>
+        </mirror>
+    </mirrors>
+</settings>
+EOF
+  log_info "***** End to config maven *****"
+}
+
+#配置华为云gradle源
+function config_gradle() {
+  log_info "***** Start to config gradle *****"
+  cat >${JENKINS_HOME}/.gradle <<EOF
+allprojects {
+    buildscript {
+        repositories {
+            maven { url "https://repo.huaweicloud.com/repository/maven/" }
+
+        }
+    }
+    repositories {
+        maven { url "https://repo.huaweicloud.com/repository/maven/" }
+
+    }
+}
+EOF
+  log_info "***** End to config gradle *****"
+}
+
+# 启动ipv6 loopback
+function config_ipv6() {
+  log_info "***** Start to config ipv6 *****"
+  sudo sysctl net.ipv6.conf.lo.disable_ipv6=0 &>/dev/null
+  sudo su - root <<FEOF
+text=\$(cat /etc/resolv.conf)
+echo "nameserver 100.125.1.250" > /etc/resolv.conf
+for i in \$text; do
+	echo \$i >> /etc/resolv.conf
+done
+FEOF
+  log_info "***** End to config ipv6 *****"
+}
+
+# 从src-openeuler下载kernel代码
+function download_kernel_repo_soe() {
+  log_info "***** Start to download kernel of src-openeuler *****"
+  git init kernel
+  cd kernel
+  git fetch --depth 4 https://gitee.com/src-openeuler/kernel +refs/pull/${prid}/MERGE:pr_${prid}
+  git checkout pr_${prid}
+  cd ../
+  log_info "***** End to download kernel of src-openeuler *****"
+}
+
+download_kernel_times=0 # 避免下载多次kernel
+# 下载kernel代码
+function download_kernel_repo() {
+  log_info "***** Start to download kernel of openeuler *****"
+  if [[ $download_kernel_times -gt 0 ]]; then
+    log_info "already clone kernel source"
+    return
+  fi
+
+  ((download_kernel_times += 1))
+
+  if [[ -d code/kernel ]]; then
+    rm -rf code/kernel
+  fi
+
+  if [ "x$repo" == "xkernel" ]; then
+    download_kernel_repo_soe
+    kernel_tag=$(cat kernel/SOURCE)
+    log_info "now clone kernel source of tag ${kernel_tag} to code/kernel"
+    git clone -b $kernel_tag --depth 1 https://${GiteeUserName}:${GiteePassword}@gitee.com/openeuler/kernel code/kernel
+  else
+    log_info "now clone kernel source of branch ${tbranch}"
+    git clone -b $tbranch --depth 1 https://${GiteeUserName}:${GiteePassword}@gitee.com/openeuler/kernel code/kernel
+  fi
+
+  # 处理 "Installed (but unpackaged) file" 异常
+  rm -rf $WORKSPACE/code/kernel/.git
+  log_info "***** End to download kernel of openeuler *****"
+}
+
+# 下载关联仓库代码，某些仓库编译依赖其他仓库代码
+function download_buddy_repo() {
+  log_info "***** Start to download buddy rpm *****"
+  # download buddy repo
+  for item in $(echo ${buddy} | sed 's/,/ /g'); do
+    # TODO
+    echo "############## repo is ${repo} item is ${item}"
+    #对kernel特殊处理
+    if [[ "x$item" == "xkernel" ]]; then
+      download_kernel_repo
+    elif [[ "x$item" != "x$repo" ]]; then
+      log_info "clone ${item} of branch $tbranch"
+      git clone -b $tbranch --depth 1 https://${GiteeUserName}:${GiteePassword}@gitee.com/src-openeuler/${item}
+    fi
+  done
+  log_info "***** End to download buddy rpm *****"
+}
+
+# 防止复用pod，强制清除一些文件和目录
+function drop_pod_cache() {
+  log_info "***** Start to clean pod env *****"
+  rm -rf /tmp/local_code/xdf/repo
+
+  # 解挂buildroot目录
+  set +e
+  mount | grep buildroot | awk '{print $3}' | sudo xargs umount &>/dev/null
+  set -e
+
+  if [[ -d ${BUILD_ROOT} ]]; then
+    sudo chmod -R 777 ${BUILD_ROOT} # buildroot mode is 444, owner and group is root
+    rm -rf ${BUILD_ROOT}
+  fi
+
+  # check abi comment
+  if [[ -e $WORKSPACE/${repo}_${arch}_comment ]]; then
+    rm $WORKSPACE/${repo}_${arch}_comment
+  fi
+  if [[ -e $WORKSPACE/${repo}_${arch}_compare_result ]]; then
+    rm $WORKSPACE/${repo}_${arch}_compare_result
+  fi
+  log_info "***** End to clean pod env *****"
+}
+
+# 编译软件包和执行install
+function build_packages() {
+  log_info "***** Start to build package *****"
+  comment_file="${repo}_${arch}_comment"
+  export PYTHONPATH=${JENKINS_HOME}/ci_check
+  for item in $(echo ${package} | sed 's/,/ /g'); do
+    log_info "start build package $item"
+    log_debug "params are [$repo, $branch, $prid, $committer, $arch, $package, $buddy, $WORKSPACE]"
+
+    if [[ "x${item}" == "xkernel" ]]; then
+      python3 ${SCRIPT_PATCH}/osc_build_k8s.py -p $item -a $arch -c $WORKSPACE -b $tbranch -r ${repo} -m ${commentid} --pr ${prid} -t ${GiteeUserPassword} --spec "kernel.spec"
+    else
+      python3 ${SCRIPT_PATCH}/osc_build_k8s.py -p $item -a $arch -c $WORKSPACE -b $tbranch -r ${repo} -m ${commentid} --pr ${prid} -t ${GiteeUserPassword}
+    fi
+
+    log_debug "check install"
+    python3 ${SCRIPT_PATCH}/extra_work.py checkinstall -a ${arch} -r $tbranch --install-root=${JENKINS_HOME}/install_root -e $WORKSPACE/${comment_file} || echo "continue although run check install failed"
+
+    log_debug "pkgship notify"
+    if [[ "x$item" == "xpkgship" ]]; then
+      scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:/repo/soe/pkgship pkgship_notify
+      python3 ${SCRIPT_PATCH}/extra_work.py notify -p ${repo} -a ${arch} -n ${pkgship_notify_url} -t ${pkgship_notify_token} -l "http://${repo_server}" -m pkgship_notify -u ${PkgShipUserName} -w ${PkgShipPassword} || echo "continue although run pkgship notify error"
+    fi
+  done
+  log_info "***** End to build package *****"
+}
+
+# 比较软件包差异
+function compare_package() {
+  log_info "***** Start to compare package diff *****"
+  old_dir="${JENKINS_HOME}/old_rpms/"
+  new_dir="${JENKINS_HOME}/new_rpms/"
+  result_dir="${JENKINS_HOME}/oecp_result"
+  if [[ -d $old_dir ]]; then
+    rm -rf $old_dir
+  fi
+  if [[ -d $new_dir ]]; then
+    rm -rf $new_dir
+  fi
+  if [[ -d $result_dir ]]; then
+    rm -rf $result_dir
+  fi
+  mkdir -p $old_dir
+  mkdir -p $new_dir
+  mkdir -p $result_dir
+
+  if [[ -d ${RPM_PATH}/${arch} && "$(ls -A ${RPM_PATH}/${arch})" ]]; then
+    cp ${RPM_PATH}/${arch}/*.rpm $new_dir
+  fi
+
+  if [[ -d ${RPM_PATH}/noarch && "$(ls -A ${RPM_PATH}/noarch)" ]]; then
+    cp ${RPM_PATH}/noarch/*.rpm $new_dir
+  fi
+
+  if [[ $(ssh -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR root@${repo_server} test -e "root@${repo_server}:/repo/openeuler/src-openeuler/${tbranch}/0X080480000XC0000000/${repo}/${arch}/") ]]; then
+    log_info "try download rpms from ci server"
+    scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR root@${repo_server}:/repo/openeuler/src-openeuler/${tbranch}/0X080480000XC0000000/${repo}/${arch}/*.rpm $old_dir
+  fi
+  if [[ ! "$(ls -A $old_dir | grep '.rpm')" ]]; then
+    log_info "try download rpms from obs server"
+    python3 ${SCRIPT_PATCH}/extra_work.py getrelatedrpm -r $tbranch -p ${item} -a ${arch} || echo "continue although run get related rpm failed"
+    if [[ -d binaries && "$(ls -A binaries | grep '\.rpm$')" ]]; then
+      cp binaries/*.rpm $old_dir
+    fi
+  fi
+
+  python3 ${JENKINS_HOME}/oecp/cli.py $old_dir $new_dir -o $result_dir -w $result_dir -n 2 -f json || echo "continue although run oecp failed"
+
+  pr_link='https://gitee.com/src-openeuler/'${repo}'/pulls/'${prid}
+  pr_commit_json_file="${JENKINS_HOME}/pr_commit_json_file"
+  curl https://gitee.com/api/v5/repos/src-openeuler/${repo}/pulls/${prid}/files?access_token=$GiteeToken >$pr_commit_json_file
+  compare_result="${repo}_${arch}_compare_result"
+
+  if [[ ! "$(ls -A $old_dir | grep '.rpm')" ]]; then
+    echo "this is first commit PR"
+    python3 ${SCRIPT_PATCH}/extra_work.py comparepackage --ignore -p ${repo} -j $result_dir/report-$old_dir-$new_dir/osv.json -pr $pr_link -pr_commit $pr_commit_json_file -f $WORKSPACE/${compare_result} || echo "continue although run compare package failed"
+  else
+    python3 ${SCRIPT_PATCH}/extra_work.py comparepackage -p ${repo} -j $result_dir/report-$old_dir-$new_dir/osv.json -pr $pr_link -pr_commit $pr_commit_json_file -f $WORKSPACE/${compare_result} || echo "continue although run compare package failed"
+  fi
+
+  # run before save rpm, reset remote dir
+  fileserver_user_path="/repo/openeuler/src-openeuler/${tbranch}/${committer}/${repo}/${arch}/${prid}"
+  fileserver_tmpfile_path="/repo/soe/check_item"
+  remote_dir_reset_cmd=$(
+    cat <<EOF
+if [[ ! -d "$fileserver_user_path" ]]; then
+	mkdir -p $fileserver_user_path
+fi
+if [[ \$(ls -A "$fileserver_user_path" | grep ".rpm") ]]; then
+	rm $fileserver_user_path/*.rpm
+fi
+if [[ \$(ls -A "$fileserver_user_path" | grep ".json") ]]; then
+	rm $fileserver_user_path/*.json
+fi
+if [[ ! -d "$fileserver_tmpfile_path" ]]; then
+	mkdir -p $fileserver_tmpfile_path
+fi
+if [[ -e "$fileserver_tmpfile_path/${compare_result}" ]]; then
+	rm $fileserver_tmpfile_path/${compare_result}
+fi
+if [[ -e "$fileserver_tmpfile_path/${comment_file}" ]]; then
+	rm $fileserver_tmpfile_path/${comment_file}
+fi
+EOF
+  )
+  #echo "$remote_dir_reset_cmd"
+  ssh -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR root@${repo_server} "$remote_dir_reset_cmd"
+
+  log_info "save result"
+  if [[ -e $result_dir/report-$old_dir-$new_dir/osv.json ]]; then
+    old_any_rpm=$(ls $old_dir | head -n 1)
+    old_version=$(rpm -q $old_dir/$old_any_rpm --queryformat '%{version}\n')
+    old_release=$(rpm -q $old_dir/$old_any_rpm --queryformat '%{release}\n')
+    old_release=${old_release%%\.oe1}
+    new_any_rpm=$(ls $new_dir | head -n 1)
+    new_version=$(rpm -q $new_dir/$new_any_rpm --queryformat '%{version}\n')
+    new_release=$(rpm -q $new_dir/$new_any_rpm --queryformat '%{release}\n')
+    new_release=${new_release%%\.oe1}
+
+    new_json_name=${repo}_${old_version}-${old_release}_${new_version}-${new_release}.json
+    scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR $result_dir/report-$old_dir-$new_dir/osv.json root@${repo_server}:/repo/openeuler/src-openeuler/${tbranch}/${committer}/${repo}/${arch}/${prid}/$new_json_name
+  fi
+  if [[ -d $new_dir && "$(ls -A $new_dir | grep '.rpm')" ]]; then
+    scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR $new_dir/* root@${repo_server}:/repo/openeuler/src-openeuler/${tbranch}/${committer}/${repo}/${arch}/${prid}/
+  fi
+  if [[ -e $compare_result ]]; then
+    scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR ${compare_result} root@${repo_server}:$fileserver_tmpfile_path/${compare_result}
+  fi
+  if [[ -e $comment_file ]]; then
+    scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR ${comment_file} root@${repo_server}:$fileserver_tmpfile_path/${comment_file}
+  fi
+  log_info "***** End to compare package diff *****"
+}
+
+# 执行入口
+function main() {
+  config_osc
+  config_ipv6
+  config_maven
+  config_gradle
+  download_buddy_repo
+  drop_pod_cache
+  build_packages
+  compare_package
+}
