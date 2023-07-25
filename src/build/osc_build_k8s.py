829acfdc5095626dd9766d7aef75f767b8a97aa9
wanghuan158 <wanghuan3456@163.com>
2023-07-25 11:04:25 +0800

    fuse3 add root

--- src/build/osc_build_k8s.py
+++ src/build/osc_build_k8s.py
@@ -32,7 +32,7 @@ class SinglePackageBuild(object):
     """
     BUILD_IGNORED_GITEE_BRANCH = ["riscv"]
     PACKAGES_USE_ROOT = ["iproute", "libaio", "A-Ops", "multipath-tools", "libnetfilter_conntrack", "mcelog",
-                         "openEuler_chroot", "conntrack-tools", "linux-sgx", "gala-gopher", "iputils"]
+                         "openEuler_chroot", "conntrack-tools", "linux-sgx", "gala-gopher", "iputils", "fuse3"]
 
     def __init__(self, package, arch, target_branch):
         """
