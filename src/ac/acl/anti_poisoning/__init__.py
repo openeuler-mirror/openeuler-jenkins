d133af8b4d7bc2d6d29a673d1f88e9b50826d069
wanghuan158 <wanghuan3456@163.com>
2024-01-22 17:18:38 +0800

    add anti-poisoning check

---
+++ src/ac/acl/anti_poisoning/__init__.py
@@ -0,0 +1,18 @@
+# -*- encoding=utf-8 -*-
+"""
+# **********************************************************************************
+# Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
+# [openeuler-jenkins] is licensed under the Mulan PSL v1.
+# You can use this software according to the terms and conditions of the Mulan PSL v1.
+# You may obtain a copy of Mulan PSL v1 at:
+#     http://license.coscl.org.cn/MulanPSL
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v1 for more details.
+# Author:
+# Create: 2024-01-19
+# Description: check anti_poisoning
+# **********************************************************************************
+"""
+
