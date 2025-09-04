cd9a4512ab3ac9d41f14c76c2647841851697d52
wangyangdahai <wangyang22@iscas.ac.cn>
2025-09-04 18:40:24 +0800

    add riscv64 arch for gitee commit comment.

--- src/lib/comment.sh
+++ src/lib/comment.sh
@@ -3,10 +3,13 @@
 
 check_item_comment_aarch64=""
 check_item_comment_x86=""
+check_item_comment_riscv64=""
 compare_package_result_aarch64=""
 compare_package_result_x86=""
+compare_package_result_riscv64=""
 detail_result_file_aarch64=""
 detail_result_file_x86_64=""
+detail_result_file_riscv64=""
 
 repo_server_test_tail=""
 token=${giteetoken}
@@ -34,9 +37,11 @@ function clearn_env() {
   # download compare package comment files
   check_item_comment_aarch64="${repo}_${prid}_aarch64_comment"
   check_item_comment_x86="${repo}_${prid}_x86_64_comment"
+  check_item_comment_riscv64="${repo}_${prid}_riscv64_comment"
   #cat $compare_package_comment_x86
   compare_package_result_aarch64="${repo}_${prid}_aarch64_compare_result"
   compare_package_result_x86="${repo}_${prid}_x86_64_compare_result"
+  compare_package_result_riscv64="${repo}_${prid}_riscv64_compare_result"
   build_num_file="${repo_owner}_${repo}_${prid}_build_num.yaml"
 
 
@@ -46,11 +51,17 @@ function clearn_env() {
   if [[ -e $check_item_comment_x86 ]]; then
     rm $check_item_comment_x86
   fi
+  if [[ -e $check_item_comment_riscv64 ]]; then
+    rm $check_item_comment_riscv64
+  fi
   if [[ -e $compare_package_result_aarch64 ]]; then
     rm $compare_package_result_aarch64
   fi
   if [[ -e $compare_package_result_x86 ]]; then
     rm $compare_package_result_x86
+  fi
+  if [[ -e $compare_package_result_riscv64 ]]; then
+    rm $compare_package_result_riscv64
   fi
   if [[ -e build_num_file ]]; then
     rm $build_num_file
@@ -64,13 +75,17 @@ function scp_comment_file() {
   fileserver_tmpfile_path="/repo/soe${repo_server_test_tail}/check_item"
   detail_result_file_aarch64="${repo}_aarch64.json"
   detail_result_file_x86_64="${repo}_x86_64.json"
+  detail_result_file_riscv64="${repo}_riscv64.json"
   scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${check_item_comment_aarch64} . || log_info "file ${check_item_comment_aarch64} not exist"
   scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${check_item_comment_x86} . || log_info "file ${check_item_comment_x86} not exist"
+  scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${check_item_comment_riscv64} . || log_info "file ${check_item_comment_riscv64} not exist"
   #ls $WORKSPACE/${comment}
   scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:"/repo/openeuler/src-openeuler${repo_server_test_tail}/${tbranch}/${committer}/${repo}/aarch64/${prid}/${repo}_*.json" ${detail_result_file_aarch64} || log_info "file ${detail_result_file_aarch64} not exist"
   scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:"/repo/openeuler/src-openeuler${repo_server_test_tail}/${tbranch}/${committer}/${repo}/x86_64/${prid}/${repo}_*.json" ${detail_result_file_x86_64} || log_info "file ${detail_result_file_x86_64} not exist"
+  scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:"/repo/openeuler/src-openeuler${repo_server_test_tail}/${tbranch}/${committer}/${repo}/riscv64/${prid}/${repo}_*.json" ${detail_result_file_riscv64} || log_info "file ${detail_result_file_riscv64} not exist"
   scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${compare_package_result_aarch64} . || log_info "file ${compare_package_result_aarch64} not exist"
   scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${compare_package_result_x86} . || log_info "file ${compare_package_result_x86} not exist"
+  scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${compare_package_result_riscv64} . || log_info "file ${compare_package_result_riscv64} not exist"
   ls $WORKSPACE/${compare_result}
   scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:$fileserver_tmpfile_path/${build_num_file} . || log_info "file ${build_num_file} not exist"
   scp -r -i ${SaveBuildRPM2Repo} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${repo_server}:/repo/soe${repo_server_test_tail}/support_arch/${repo}_${prid}_support_arch .
@@ -86,9 +101,9 @@ function exec_comment() {
   url_files_server="http://${repo_server}/src-openeuler${repo_server_test_tail}/${tbranch}/${committer}/${repo}/replace__arch/${prid}"
   export PYTHONPATH=${shell_path}
   python3 ${shell_path}/src/build/gitee_comment.py -o $repo_owner -r $repo -p $prid -c $committer -t ${token}\
-   -b $jenkins_api_host -u $jenkins_user -j $jenkins_api_token -a ${check_item_comment_aarch64} ${check_item_comment_x86}\
-    -f ${compare_package_result_x86},${compare_package_result_aarch64} -m ${commentid} -l ${url_files_server} \
-    -d ${detail_result_file_x86_64},${detail_result_file_aarch64} -tb ${tbranch} --platform ${platform}
+   -b $jenkins_api_host -u $jenkins_user -j $jenkins_api_token -a ${check_item_comment_aarch64} ${check_item_comment_x86} ${check_item_comment_riscv64}\
+    -f ${compare_package_result_x86},${compare_package_result_aarch64},${compare_package_result_riscv64} -m ${commentid} -l ${url_files_server} \
+    -d ${detail_result_file_x86_64},${detail_result_file_aarch64},${detail_result_file_riscv64} -tb ${tbranch} --platform ${platform}
   log_info "***** End to exec comment *****"
   log_info "***** Start to exec comment to kafka*****"
   python3 ${shell_path}/src/build/comment_to_dashboard.py -r $repo -c $committer -m ${commentid} -g $jobtriggertime\
