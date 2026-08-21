1129ea7b7ef7b34f174210be71c9f293a03b12a0
xiaoniuzi <dingxudong1@huawei.com>
2026-08-21 17:06:26 +0800

    删除文档和测试代码中的敏感信息

--- test/ac/acl/openlibing/test_openlibing.py
+++ test/ac/acl/openlibing/test_openlibing.py
@@ -16,42 +16,38 @@
 # **********************************************************************************
 """
 
-
-import unittest
 import logging
-
+import unittest
 from unittest import mock
 
-from src.ac.framework.ac_result import FAILED, SUCCESS
-from src.ac.acl.sca.check_sca import CheckSCA
-from src.ac.acl.openlibing.check_code import CheckCode
 from src.ac.acl.anti_poisoning.check_anti_poisoning import CheckAntiPoisoning
+from src.ac.acl.openlibing.check_code import CheckCode
+from src.ac.acl.sca.check_sca import CheckSCA
+from src.ac.framework.ac_result import FAILED, SUCCESS
 
-logging.getLogger('test_logger')
+logging.getLogger("test_logger")
 
 
 class TestCheckCode(unittest.TestCase):
-
-    OPENLIBING_ARG = {"pr_url": "https://gitee.com/openeuler/iSulad/pulls/2624",
-                "community": "openeuler", "pr_num": "2624",
-                "accountid": "02657278-f08a-4169-b80f-1e6626b6b72d",
-                "secretKey": "qCi6cHEgValRUA1oFpz8AnvnRUupWGJLyrQr135IP+A=",
-                "access_token": "44c4948c0ed31fcc8b53c2f6f05bc2a4"}
-
+    OPENLIBING_ARG = {
+        "pr_url": "https://gitee.com/openeuler/iSulad/pulls/2624",
+        "community": "openeuler",
+        "pr_num": "2624",
+    }
 
     def _test_codecheck_api(self, return_value):
         mock_fun = mock.Mock(return_value=return_value)
-        cc = CheckCode('./test', self.OPENLIBING_ARG.get("pr_url"))
+        cc = CheckCode("./test", self.OPENLIBING_ARG.get("pr_url"))
         self.assertEqual(cc(common_args=self.OPENLIBING_ARG), mock_fun())
 
     def _test_anti_poison_api(self, return_value):
         mock_fun = mock.Mock(return_value=return_value)
-        ap = CheckAntiPoisoning('./test', self.OPENLIBING_ARG.get("pr_url"))
+        ap = CheckAntiPoisoning("./test", self.OPENLIBING_ARG.get("pr_url"))
         self.assertEqual(ap(common_args=self.OPENLIBING_ARG), mock_fun())
 
     def _test_sca_api(self, return_value):
         mock_fun = mock.Mock(return_value=return_value)
-        cc = CheckSCA('./test', self.OPENLIBING_ARG.get("pr_url"))
+        cc = CheckSCA("./test", self.OPENLIBING_ARG.get("pr_url"))
         self.assertEqual(cc(common_args=self.OPENLIBING_ARG), mock_fun())
 
     def test_codecheck_api_success(self):
@@ -60,7 +56,7 @@ class TestCheckCode(unittest.TestCase):
 
     def test_codecheck_api_failed_no_pr_url(self):
         return_value = FAILED
-        self.OPENLIBING_ARG['pr_url'] = ""
+        self.OPENLIBING_ARG["pr_url"] = ""
         self._test_codecheck_api(return_value)
 
     def test_anti_poison_api_success(self):
@@ -69,7 +65,7 @@ class TestCheckCode(unittest.TestCase):
 
     def test_anti_poison_api_failed_no_pr_url(self):
         return_value = FAILED
-        self.OPENLIBING_ARG['pr_url'] = ""
+        self.OPENLIBING_ARG["pr_url"] = ""
         self._test_anti_poison_api(return_value)
 
     def test_sca_api_success(self):
@@ -78,5 +74,5 @@ class TestCheckCode(unittest.TestCase):
 
     def test_sca_api_failed_no_pr_url(self):
         return_value = FAILED
-        self.OPENLIBING_ARG['pr_url'] = ""
+        self.OPENLIBING_ARG["pr_url"] = ""
         self._test_anti_poison_api(return_value)
